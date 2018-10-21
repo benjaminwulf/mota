f373612a71e743b4c4722bc28014a22697530833
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-10-20 23:48:57 -0600

    buttons for effect and flavor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Navigation from "./components/Navigation/Navigation";
 // import Search from "./components/Search/Search";
-// import EffectFlavor from "./components/EffectFlavor/EffectFlavor";
+import EffectFlavor from "./components/EffectFlavor/EffectFlavor";
 // import StrainDisplay from ".//components/StrainDisplay";
 
 const App = () => (
   <div>
        <Navigation />
-      {/* <Search />
-      <EffectFlavor />
-      <StrainDisplay /> */}
+      {/* <Search /> */}
+       <EffectFlavor />
+      {/* <StrainDisplay /> */}
   </div>
 );
 
--- src/components/EffectFlavor/EffectFlavor.js
+++ src/components/EffectFlavor/EffectFlavor.js
@@ -1,4 +1,258 @@
 import React from "react";
 import "./EffectFlavor.css";
+import { Grid, Row, Col, Panel, Button } from 'react-bootstrap';
+
+const EffectFlavor = () => (
+<Grid>
+    <Row>
+        <Col xs={4} md={2}>
+            <h1>Effect</h1>
+            <Panel bsStyle="info">
+            <Panel.Heading>
+              <Panel.Title componentClass="h3">Relaxed</Panel.Title>
+              </Panel.Heading>
+              <Panel.Body>positive</Panel.Body>
+            </Panel>
+            <Panel bsStyle="danger">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Dizzy</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>negative</Panel.Body>
+            </Panel>
+            <Panel bsStyle="info">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Hugry</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>poitive</Panel.Body>
+            </Panel>
+            <Panel bsStyle="info">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Happy</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>positive</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Depression</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Insomnia</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Pain</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Stress</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Cramps</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Creative</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>positive</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Energetic</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>positive</Panel.Body>
+            </Panel>
+            <Panel bsStyle="info">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Talkative</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>positive</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Lack of Appetite</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Nausea</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="danger">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Dry Mouth</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>negative</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Headache</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="info">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Uplifted</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>positive</Panel.Body>
+            </Panel>
+            <Panel bsStyle="info">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Tingly</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>positive</Panel.Body>
+            </Panel>
+            <Panel bsStyle="danger">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Paranoid</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>negative</Panel.Body>
+            </Panel>
+            <Panel bsStyle="info">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Sleepy</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>positive</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Fatigue</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Headaches</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Dry Eyes</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>negative</Panel.Body>
+            </Panel>
+            <Panel bsStyle="info">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Focused</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>positive</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Eye Pressure</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="danger">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Anxious</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>negative</Panel.Body>
+            </Panel>
+            <Panel bsStyle="info">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Giggly</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>positive</Panel.Body>
+            </Panel>
+            <Panel bsStyle="info">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Aroused</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>positive</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Inflammation</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Spasticity</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Seizures</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+            <Panel bsStyle="success">
+              <Panel.Heading>
+              <Panel.Title componentClass="h3">Muscle Spasms</Panel.Title>
+              </Panel.Heading>
+            <Panel.Body>medical</Panel.Body>
+            </Panel>
+        </Col>
+        <Col xs={2} md={2}>
+            <h1>Flavor</h1>
+            <Button bsStyle="warning">Earthy</Button>
+            <Button bsStyle="warning">Chemical</Button>
+            <Button bsStyle="warning">Pine</Button>
+            <Button bsStyle="warning">Spicy/Herbal</Button>
+            <Button bsStyle="warning">Pungent</Button>
+            <Button bsStyle="warning">Pepper</Button>
+            <Button bsStyle="warning">Flowery</Button>
+            <Button bsStyle="warning">Citrus</Button>
+            <Button bsStyle="warning">Orange</Button>
+            <Button bsStyle="warning">Sweet</Button>
+            <Button bsStyle="warning">Skunk</Button>
+            <Button bsStyle="warning">Grape</Button>
+            <Button bsStyle="warning">Minty</Button>
+            <Button bsStyle="warning">Woody</Button>
+            <Button bsStyle="warning">Cheese</Button>
+            <Button bsStyle="warning">Diesel</Button>
+            <Button bsStyle="warning">Tropical</Button>
+            <Button bsStyle="warning">Grapefruit</Button>
+            <Button bsStyle="warning">Nutty</Button>
+            <Button bsStyle="warning">Lemon</Button>
+            <Button bsStyle="warning">Berry</Button>
+            <Button bsStyle="warning">Blueberry</Button>
+            <Button bsStyle="warning">Ammonia</Button>
+            <Button bsStyle="warning">Apple</Button>
+            <Button bsStyle="warning">Rose</Button>
+            <Button bsStyle="warning">Butter</Button>
+            <Button bsStyle="warning">Honey</Button>
+            <Button bsStyle="warning">Tea</Button>
+            <Button bsStyle="warning">Lime</Button>
+            <Button bsStyle="warning">Lavender</Button>
+            <Button bsStyle="warning">Strawberry</Button>
+            <Button bsStyle="warning">Mint</Button>
+            <Button bsStyle="warning">Chestnut</Button>
+            <Button bsStyle="warning">Tree Fruit</Button>
+            <Button bsStyle="warning">Pear</Button>
+            <Button bsStyle="warning">Apricot</Button>
+            <Button bsStyle="warning">Peach</Button>
+            <Button bsStyle="warning">Blue Cheese</Button>
+            <Button bsStyle="warning">Menthol</Button>
+            <Button bsStyle="warning">Coffee</Button>
+            <Button bsStyle="warning">Tar</Button>
+            <Button bsStyle="warning">Mango</Button>
+            <Button bsStyle="warning">Pineapple</Button>
+            <Button bsStyle="warning">Sage</Button>
+            <Button bsStyle="warning">Vanilla</Button>
+            <Button bsStyle="warning">Plum</Button>
+            <Button bsStyle="warning">Tobacco</Button>
+            <Button bsStyle="warning">Violet</Button>
+        </Col>
+    </Row>
+</Grid>
+);
 
 export default EffectFlavor;
